28f3470bd0d7ff70ed41bca495cd2781523533fa
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-10-30 12:12:03 +0100

    finish

--- ejercicio.js
+++ ejercicio.js
@@ -61,6 +61,49 @@ gente.forEach(persona => {
 
 //MAP
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
+//3.1
+let personas25 = gente.map(persona => persona.edad >25);
+console.log(personas25);
+
+
+//3.2
+let personasJ = gente.map(persona => {
+    if (persona.nombre.charAt(0) === "J"){
+    return persona;
+    }else{
+        return null;
+    }
+}).filter(Boolean);
+console.log(personasJ);
+
+//3.3
+const numbers = [4, 5, 6, 7, 8, 9, 10];
+const numberPow = gente.map(number => Math.pow(number,number));
+console.log(numberPow);
+
+//Filter
+//4.1
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+let numberImp = numbers2.filter(number => number % 2 !== 0);
+console.log(numberImp);
+
+//4.2
+
+ const foodList = [
+        { name: 'Tempeh', isVeggie: true },
+        { name: 'Cheesbacon burguer', isVeggie: false },
+        { name: 'Tofu burguer', isVeggie: true },
+        { name: 'Entrecot', isVeggie: false }
+      ];
+
+let food2 = foodList.filter(food => food.isVeggie).map(veganFood => `Que rico ${veganFood.name} me voy a comer`);
+console.log(food2);
+
+//REDUCE
+//5.1
+const numbers3 = [39, 2, 4, 25, 62];
+
+const sum = numbers3.reduce((a, b) => a * b, 1);
+console.log(sum);
